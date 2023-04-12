ae2cb083a58356081a6d71c6dff38c8721296c54
gitlkd <129717105+gitlkd@users.noreply.github.com>
2023-04-12 13:56:30 +0800

    Add files via upload

--- perclos.py
+++ perclos.py
@@ -3,147 +3,145 @@ import cv2
 import dlib
 import numpy as np
 from scipy.spatial import distance as dist
-detector = dlib.get_frontal_face_detector()
-# 定义一些常量
-EYE_AR_THRESH = 0.3 # 眼部纵横比的阈值，低于该值则认为眼睛闭合
-EYE_AR_CONSEC_FRAMES = 3 # 连续多少帧低于阈值则认为眨眼
-BLINK_DURATION = 0.1 # 每次眨眼的持续时间，单位为秒
-P80_THRESH = 0.2 # perclos的p80阈值，高于该值则认为出现疲劳驾驶
-FATIGUE_ALERT_DURATION = 2 # 疲劳驾驶提醒的持续时间，单位为秒
 
+# 定义一些常量或参数
+EYE_AR_THRESH = 0.2 # 眼部纵横比阈值，用于判断眼睛是否闭合
+EYE_AR_CONSEC_FRAMES = 3 # 连续多少帧低于阈值才认为眼睛闭合
+BLINK_DURATION = 0.1 # 假设每次眨眼持续0.1秒
+P80_THRESH = 0.012 # p80阈值，用于判断是否出现疲劳驾驶
+
+# 加载dlib库中的shape_predictor
+predictor = dlib.shape_predictor("D:\shiyan\p\shape_predictor_68_face_landmarks.dat")
+# 加载dlib库中的face_detector
+detector = dlib.get_frontal_face_detector()
 # 定义一个函数来计算眼部纵横比
 def eye_aspect_ratio(eye):
-    # 计算两组垂直眼睛标志（x，y） - 坐标之间的欧氏距离
+    # 计算眼睛两个垂直方向上的距离
     A = dist.euclidean(eye[1], eye[5])
     B = dist.euclidean(eye[2], eye[4])
-
-    # 计算水平之间的欧氏距离
-    # 眼睛标志（x，y） - 坐标
+    # 计算眼睛水平方向上的距离
     C = dist.euclidean(eye[0], eye[3])
-
-    # 计算眼睛的纵横比
+    # 计算眼部纵横比
     ear = (A + B) / (2.0 * C)
-
-    # 返回眼睛的纵横比
     return ear
 
-# 定义一个函数来检测眼睛状态并返回计数器和总眨眼次数
+# 定义一个函数来计算perclos值
+def perclos(blink_count, total_frames, fps):
+    # 计算眨眼总时间
+    blink_duration = blink_count * BLINK_DURATION
+    # 计算视频总时间
+    total_duration = total_frames / fps
+    # 计算perclos值
+    perclos_value = blink_duration / total_duration
+    return perclos_value
+
+# 定义一个函数来检测眼睛状态并返回perclos值和疲劳驾驶标志
 def detect_eye_state(frame, counter, total):
-    # 将图像转换为灰度图像
+    # 转换为灰度图像
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # 使用dlib库检测人脸
-    rects = detector(gray, 0)
-
-    # 遍历每一个检测到的人脸
-    for rect in rects:
-        # 获取人脸区域的坐标和大小
-        (x, y, w, h) = rect_to_bb(rect)
-        # 绘制人脸区域矩形框
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-        # 提取人脸区域，并使用预测器获取68个特征点
-        shape = predictor(gray, rect)
-        shape = shape_to_np(shape)
-
-        # 提取左眼和右眼对应的特征点，并计算它们的纵横比
-        leftEye = shape[lStart:lEnd]
-        rightEye = shape[rStart:rEnd]
+    # 检测人脸
+    faces = detector(gray)
+    for face in faces:
+        # 获取人脸关键点坐标
+        landmarks = predictor(gray, face)
+        # 获取左眼和右眼的坐标点
+        leftEye = np.array([(landmarks.part(n).x, landmarks.part(n).y) for n in range(36, 42)])
+        rightEye = np.array([(landmarks.part(n).x, landmarks.part(n).y) for n in range(42, 48)])
+        # 计算左眼和右眼的眼部纵横比
         leftEAR = eye_aspect_ratio(leftEye)
         rightEAR = eye_aspect_ratio(rightEye)
-
-        # 计算两只眼睛的平均纵横比
+        # 计算两只眼睛的平均眼部纵横比
         ear = (leftEAR + rightEAR) / 2.0
-
-        # 使用cv2.convexHull函数计算左眼和右眼的凸包，然后绘制出来
+        # 用凸包函数来获取眼睛轮廓
         leftEyeHull = cv2.convexHull(leftEye)
         rightEyeHull = cv2.convexHull(rightEye)
+        # 在图像上绘制眼睛轮廓
         cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
         cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
-
-        # 检查眼部纵横比是否低于阈值，如果低于则增加计数器
+        # 判断眼部纵横比是否低于阈值
         if ear < EYE_AR_THRESH:
+            # 如果低于阈值，计数器加一
             counter += 1
-
-            # 否则，眼睛纵横比不低于阈值
         else:
-            # 如果之前的帧中眼睛是闭合的
+            # 如果高于阈值，判断计数器是否大于等于设定的帧数
             if counter >= EYE_AR_CONSEC_FRAMES:
-                # 增加总眨眼次数
+                # 如果大于等于设定的帧数，总眨眼次数加一
                 total += 1
-
-            # 重置眼睛帧计数器
+            # 重置计数器
             counter = 0
-
-            # 在图像上显示眨眼次数和眼睛纵横比
+        # 在图像上显示眨眼次数和眼部纵横比
         cv2.putText(frame, "Blinks: {}".format(total), (10, 30),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        cv2.putText(frame, "EAR: {:.4f}".format(ear), (300, 30),
+        cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        # 返回计数器，总眨眼次数，以及当前帧
+    return counter, total, frame
 
-        # 计算每秒的帧数
-    fps = cap.get(cv2.CAP_PROP_FPS)
+# 定义一个函数来计算perclos值
+def perclos(blink_count, window_duration):
+    # 计算眨眼总时间
+    blink_duration = blink_count * BLINK_DURATION
+    # 计算perclos值
+    perclos_value = blink_duration / window_duration
+    return perclos_value
 
-    # 计算每次眨眼的持续帧数
-    blink_frames = int(BLINK_DURATION * fps)
-
-    # 计算perclos值，即每分钟内闭眼时间占总时间的百分比
-    perclos_value = (counter + total * blink_frames) / (60 * fps)
-
-    # 在图像上显示perclos值和fps值
-    cv2.putText(frame, "PERCLOS: {:.4f}".format(perclos_value), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-    cv2.putText(frame, "FPS: {:.2f}".format(fps), (300, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-
-    # 检查perclos值是否高于阈值，如果高于则认为出现疲劳驾驶，并在图像上显示提醒信息
-    # 使用code blocks语法来包装代码
-    while True:  # 添加一个循环
-        if perclos_value > P80_THRESH:
-            cv2.putText(frame, "疲劳驾驶，请注意休息！", (10, 90),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-            # 设置一个定时器，让提醒信息持续一定时间
-            timer = FATIGUE_ALERT_DURATION * fps
-        else:
-            timer = max(timer - 1, 0)
-
-        # 如果定时器不为零，则继续显示提醒信息
-        if timer > 0:
-            cv2.putText(frame, "疲劳驾驶，请注意休息！", (10, 90),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-
-        # 显示图像
-        cv2.imshow("Frame", frame)
-
-        # 等待按键输入，如果是q键则退出循环
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord("q"):
-            break  # 这里的break是跳出循环
-# 创建一个视频捕捉对象，并打开摄像头
+# 创建一个视频捕捉对象，参数为摄像头编号或视频文件路径
 cap = cv2.VideoCapture(0)
 
-# 使用code blocks语法来包装代码
-# 初始化一个计数器和总眨眼次数
+# 获取视频的帧率
+fps = cap.get(cv2.CAP_PROP_FPS)
+
+# 初始化计数器和总眨眼次数
 counter = 0
 total = 0
 
-# 定义fps变量，表示每秒帧数
-fps = 30 # 这里可以根据你的摄像头实际情况修改
+# 初始化perclos值和疲劳驾驶标志
+perclos_value = 0.0
+fatigue_driving = False
 
-# 初始化一个定时器，用于显示疲劳驾驶提醒信息
-timer = FATIGUE_ALERT_DURATION * fps
+# 定义一个时间窗口的大小和起始时间，单位为秒
+window_size = 1.0
+window_start = 0.0
 
-# 创建一个循环，不断从摄像头获取图像并处理
+# 循环读取视频帧，直到视频结束或按下q键退出
 while True:
-    # 获取一帧图像，并检查是否成功获取
+    # 获取一帧图像，ret为布尔值，表示是否成功获取，frame为图像数组
     ret, frame = cap.read()
     if not ret:
-        # break语句和if语句对齐，比while语句多一个缩进
         break
 
-    # 调用detect_eye_state函数，传入图像，计数器和总眨眼次数，并获取返回值
-    counter, total = detect_eye_state(frame, counter, total)
+    # 调用函数检测眼睛状态并返回计数器和总眨眼次数
+    counter, total, frame = detect_eye_state(frame, counter, total)
 
-# 释放摄像头资源
+    # 获取当前视频的时间位置，单位为秒
+    current_time = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000.0
+
+    # 判断当前时间是否超过窗口的结束时间
+    if current_time > window_start + window_size:
+        # 如果超过窗口的结束时间，计算perclos值，并重置窗口的起始时间和眨眼次数
+        perclos_value = perclos(total, window_size)
+        window_start = current_time
+        total = 0
+
+        # 判断perclos值是否超过p80阈值，如果超过则认为出现疲劳驾驶，并在图像上显示警告信息
+        if perclos_value > P80_THRESH:
+            fatigue_driving = True
+            cv2.putText(frame, "Fatigue Driving!", (100, 200), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 255), 3)
+        else:
+            fatigue_driving = False
+
+    # 在图像上显示perclos值和帧率
+    cv2.putText(frame, "PERCLOS: {:.4f}".format(perclos_value), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    cv2.putText(frame, "FPS: {:.2f}".format(fps), (300, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+    # 显示图像
+    cv2.imshow("Frame", frame)
+
+    # 等待按键输入，如果是q键则退出循环
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        break
+
+# 释放视频捕捉对象和窗口
 cap.release()
-
-# 关闭所有窗口
 cv2.destroyAllWindows()
