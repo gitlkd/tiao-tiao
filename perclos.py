9877e05ca1ef18b7765289e0d096878028693fe8
gitlkd <129717105+gitlkd@users.noreply.github.com>
2023-04-12 15:26:11 +0800

    Add files via upload

--- perclos.py
+++ perclos.py
@@ -5,15 +5,17 @@ import numpy as np
 from scipy.spatial import distance as dist
 
 # 定义一些常量或参数
-EYE_AR_THRESH = 0.2 # 眼部纵横比阈值，用于判断眼睛是否闭合
-EYE_AR_CONSEC_FRAMES = 3 # 连续多少帧低于阈值才认为眼睛闭合
-BLINK_DURATION = 0.1 # 假设每次眨眼持续0.1秒
-P80_THRESH = 0.012 # p80阈值，用于判断是否出现疲劳驾驶
+EYE_AR_THRESH = 0.2  # 眼部纵横比阈值，用于判断眼睛是否闭合
+EYE_AR_CONSEC_FRAMES = 3  # 连续多少帧低于阈值才认为眼睛闭合
+BLINK_DURATION = 0.1  # 假设每次眨眼持续0.1秒
+P80_THRESH = 0.012  # p80阈值，用于判断是否出现疲劳驾驶
 
 # 加载dlib库中的shape_predictor
 predictor = dlib.shape_predictor("D:\shiyan\p\shape_predictor_68_face_landmarks.dat")
 # 加载dlib库中的face_detector
 detector = dlib.get_frontal_face_detector()
+
+
 # 定义一个函数来计算眼部纵横比
 def eye_aspect_ratio(eye):
     # 计算眼睛两个垂直方向上的距离
@@ -25,15 +27,6 @@ def eye_aspect_ratio(eye):
     ear = (A + B) / (2.0 * C)
     return ear
 
-# 定义一个函数来计算perclos值
-def perclos(blink_count, total_frames, fps):
-    # 计算眨眼总时间
-    blink_duration = blink_count * BLINK_DURATION
-    # 计算视频总时间
-    total_duration = total_frames / fps
-    # 计算perclos值
-    perclos_value = blink_duration / total_duration
-    return perclos_value
 
 # 定义一个函数来检测眼睛状态并返回perclos值和疲劳驾驶标志
 def detect_eye_state(frame, counter, total):
@@ -52,16 +45,30 @@ def detect_eye_state(frame, counter, total):
         rightEAR = eye_aspect_ratio(rightEye)
         # 计算两只眼睛的平均眼部纵横比
         ear = (leftEAR + rightEAR) / 2.0
+
         # 用凸包函数来获取眼睛轮廓
         leftEyeHull = cv2.convexHull(leftEye)
         rightEyeHull = cv2.convexHull(rightEye)
+
         # 在图像上绘制眼睛轮廓
         cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
         cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
         # 判断眼部纵横比是否低于阈值
         if ear < EYE_AR_THRESH:
             # 如果低于阈值，计数器加一
             counter += 1
+
+            # 计算perclos值，即当前窗口内闭眼时间占总时间的比例 # 新增代码
+            perclos_value = counter * BLINK_DURATION / window_size
+
+            # 判断perclos值是否超过p80阈值，如果超过则认为出现疲劳驾驶，并在图像上显示警告信息
+            if perclos_value > P80_THRESH:
+                fatigue_driving = True
+                # 在图像上显示中文警告信息，使用putTextZH函数 # 修改代码
+                putTextZH(frame, "疲劳驾驶！", (100, 200), "simhei", 1.5, (0, 0, 255), 3)
+            else:
+                fatigue_driving = False
         else:
             # 如果高于阈值，判断计数器是否大于等于设定的帧数
             if counter >= EYE_AR_CONSEC_FRAMES:
@@ -69,21 +76,25 @@ def detect_eye_state(frame, counter, total):
                 total += 1
             # 重置计数器
             counter = 0
+
         # 在图像上显示眨眼次数和眼部纵横比
-        cv2.putText(frame, "Blinks: {}".format(total), (10, 30),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        # 返回计数器，总眨眼次数，以及当前帧
+        # cv2.putText(frame, "Blinks: {}".format(total), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2) # 删除代码
+        cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+    # 返回计数器，总眨眼次数，以及当前帧
     return counter, total, frame
 
-# 定义一个函数来计算perclos值
-def perclos(blink_count, window_duration):
-    # 计算眨眼总时间
-    blink_duration = blink_count * BLINK_DURATION
-    # 计算perclos值
-    perclos_value = blink_duration / window_duration
-    return perclos_value
+
+# 定义一个函数来在图像上显示中文文字，使用PIL库 # 新增代码
+def putTextZH(img, text, point, fontFace, fontSize, color, thickness):
+    from PIL import ImageFont, ImageDraw, Image
+    pil_img = Image.fromarray(img)
+    draw = ImageDraw.Draw(pil_img)
+    font = ImageFont.truetype(fontFace + ".ttf", int(fontSize * 10))
+    draw.text(point, text, color, font=font)
+    img = np.array(pil_img)
+    return img
+
 
 # 创建一个视频捕捉对象，参数为摄像头编号或视频文件路径
 cap = cv2.VideoCapture(0)
@@ -118,21 +129,16 @@ while True:
 
     # 判断当前时间是否超过窗口的结束时间
     if current_time > window_start + window_size:
-        # 如果超过窗口的结束时间，计算perclos值，并重置窗口的起始时间和眨眼次数
-        perclos_value = perclos(total, window_size)
+        # 如果超过窗口的结束时间，重置窗口的起始时间和眨眼次数，并将疲劳驾驶标志设为False # 修改代码
         window_start = current_time
         total = 0
-
-        # 判断perclos值是否超过p80阈值，如果超过则认为出现疲劳驾驶，并在图像上显示警告信息
-        if perclos_value > P80_THRESH:
-            fatigue_driving = True
-            cv2.putText(frame, "Fatigue Driving!", (100, 200), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 255), 3)
-        else:
-            fatigue_driving = False
+        fatigue_driving = False
+        window_size = 1.0
 
     # 在图像上显示perclos值和帧率
     cv2.putText(frame, "PERCLOS: {:.4f}".format(perclos_value), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
     cv2.putText(frame, "FPS: {:.2f}".format(fps), (300, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
 
     # 显示图像
     cv2.imshow("Frame", frame)
